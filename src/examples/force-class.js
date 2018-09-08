65a284afd8e76f1c0ec4718cb6134cd285c886f7
Eugene Sherlaimov <sherlaimov@gmail.com>
2018-09-08 14:19:15 +0300

    node rScale and text scaling

--- src/examples/force-class.js
+++ src/examples/force-class.js
@@ -6,6 +6,7 @@ import sherlaimovData from '../../data/rootNode.json';
 import { getFriends, getFollowers } from '../dataHandler';
 
 const colors = d3.scaleOrdinal(d3.schemeDark2);
+window.d3 = d3;
 
 class ForceGraph {
   constructor(chartId) {
@@ -18,15 +19,14 @@ class ForceGraph {
   }
 
   init() {
-    this.svg
-      .call(
-        d3.zoom().on('zoom', () => {
-          this.container.attr('transform', d3.event.transform);
-        })
-      )
-      .on('dblclick.zoom', null);
+    this.zoom = d3.zoom().on('zoom', () => {
+      this.container.attr('transform', d3.event.transform);
+    });
 
-    this.link = this.container.selectAll('.link');
+    this.zoom(this.svg);
+    this.svg.on('dblclick.zoom', null);
+
+    this.link = this.container.append('g').selectAll('.link');
     this.node = this.container.selectAll('.node');
 
     this.tooltip = d3
@@ -34,7 +34,8 @@ class ForceGraph {
       .append('div')
       .classed('tooltip', true)
       .style('opacity', 0); // start invisible
-
+    // TODO
+    // https://github.com/ericsoco/d3-force-attract
     this.simulation = d3
       .forceSimulation()
       .force(
@@ -42,11 +43,18 @@ class ForceGraph {
         d3
           .forceLink()
           .id(d => d.id)
-          .distance(d => d.target.followers_count / 50)
+          .distance(d => rScale(d.target.followers_count) * 2)
           .strength(0.2)
       )
       .force('charge', d3.forceManyBody().strength(-500))
       .force('center', d3.forceCenter(this.width / 2, this.height / 2));
+    // .force('bounds', this.boxingForce.bind(this));
+    // .force("collide", d3.forceCollide(50))
+    // // .force('x', d3.forceX(this.width / 2));
+    // .force(
+    //   'collide',
+    //   d3.forceCollide((d) => d.target.followers_count / 100)
+    // );
     // .alphaTarget(1);
 
     this.simulation.on('end', () => {
@@ -59,13 +67,23 @@ class ForceGraph {
     currNode
       .append('circle')
       // .transition(t)
-      // .attr('class', () => d.screen_name)
-      .attr('r', () => d.followers_count / 100)
+      .attr('r', () => rScale(d.followers_count))
+      // .attr('r', () => d.followers_count / 100)
       .style('fill', () => colors(i));
 
     currNode
       .append('text')
       .text(() => d.screen_name)
+      // Automatic Text Sizing
+      .style('font-size', function() {
+        const textWidth = this.getComputedTextLength();
+        const parentWidth = this.parentNode.getBBox().width;
+        let scale = 16;
+        if (parentWidth > textWidth) {
+          scale = ((parentWidth - 30) / this.getComputedTextLength()) * 16;
+        }
+        return `${scale}px`;
+      })
       .attr('text-anchor', 'middle')
       .attr('y', 5);
   }
@@ -73,6 +91,13 @@ class ForceGraph {
   buildGraph(data) {
     const { nodes, links } = data;
     const t = d3.transition().duration(1000);
+    const that = this;
+
+    this.rScale = d3
+      .scaleSqrt()
+      .domain([0, d3.max(nodes, d => d.friends_count)])
+      .range([1, 50]);
+    window.rScale = this.rScale;
 
     this.link = this.link.data(links);
     this.link.exit().remove();
@@ -88,22 +113,23 @@ class ForceGraph {
     // .attr('r', 0)
     this.node.exit().remove();
 
-    this.simulation.nodes(nodes);
-
     const nodeEnter = this.node
       .enter()
       .append('g')
-      .attr('class', 'node')
-      .attr('class', d => d.screen_name)
+      .attr('class', d => `node ${d.screen_name}`)
       .each(this.appendNodes);
 
     this.node = nodeEnter.merge(this.node);
+    const rootNode = this.node.filter(node => node.screen_name === 'sherlaimov');
+    window.root = rootNode;
 
     this.node.on('dblclick', this.releaseNode);
     this.node.call(
       d3
         .drag()
-        .on('start', this.dragStarted.bind(this))
+        .on('start', function(d) {
+          ForceGraph.dragStarted(d, that.simulation, this);
+        })
         .on('drag', this.dragged.bind(this))
         .on('end', this.dragEnded.bind(this))
     );
@@ -111,10 +137,12 @@ class ForceGraph {
     // node.call(tooltip(d => d.screen_name, container));
     this.node.on('mouseover', d => this.showTooltip(d));
     this.node.on('mouseleave', d => this.hideTooltip(d));
-
+    // this.node.on('dblclick.zoom', this.center.bind(this));
     this.simulation.on('tick', this.ticked.bind(this));
+    this.simulation.nodes(nodes);
     this.simulation.force('link').links(links);
-    this.simulation.alpha(1).restart();
+    // this.zoom.translateTo(this.svg, this.width / 2, this.height / 2);
+    this.simulation.alphaTarget(0.3).restart();
   }
 
   ticked() {
@@ -127,6 +155,21 @@ class ForceGraph {
     // node.attr('cx', d => d.x).attr('cy', d => d.y);
     // since now we are dealing with the g SVG element
     this.node.attr('transform', d => `translate(${d.x}, ${d.y})`);
+  }
+
+  boxingForce() {
+    this.node.attr('transform', d => {
+      // console.log(d);
+      d.x = Math.max(d.friends_count / 100, Math.min(this.width - d.friends_count / 100, d.x));
+      d.y = Math.max(d.friends_count / 100, Math.min(this.height - d.friends_count / 100, d.y));
+      return `translate(${d.x}, ${d.y})`;
+    });
+  }
+
+  center(d) {
+    d3.event.stopPropagation();
+    console.log(d);
+    console.log(this.zoom.translateTo(this.svg, d.x, d.y));
   }
 
   showTooltip(d) {
@@ -165,8 +208,9 @@ class ForceGraph {
       .style('opacity', 0);
   }
 
-  dragStarted(d) {
-    if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+  static dragStarted(d, simulation, node) {
+    d3.select(node).raise();
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     // d.fixed = true;
     d.fx = d.x;
     d.fy = d.y;
@@ -229,11 +273,11 @@ async function getNodeData() {
 const graph = new ForceGraph('force-graph');
 async function prepareGraphData() {
   const nodes = await getNodeData();
-  const filteredNodes = nodes.filter((node, i) => i < 100);
+  const filteredNodes = nodes.filter((node, i) => i < 200);
   const rootNode = getRootNodeData();
   filteredNodes.push(rootNode);
   const links = filteredNodes.map(({ id }) => ({ target: id, source: 'sherlaimov' }));
-  console.log(`Incoming nodes total => ${filteredNodes.length}`);
+  console.log(`Incoming nodes total => ${nodes.length}`);
   graph.buildGraph({ nodes: filteredNodes, links });
 }
 prepareGraphData();
